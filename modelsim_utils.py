a3bcedc6c72600a9ef4893a62c55bc76e6384a88
Brandon Valley <bavvh8@mst.edu>
2020-02-08 16:58:48 -0600

    auto_run works

---
+++ modelsim_utils.py
@@ -0,0 +1,23 @@
+import focus_utils
+
+import time 
+import keyboard as k
+
+
+# project must be open in modelsim
+# do file must be in project dir (such that if you were to start typing it in the modelsim cmd window, you could autocomplete)
+def auto_run(do_file_name = 'run_cmd.do', run_to_pane_shift_sleep_sec = 7):
+    focus_utils.set_foreground("ModelSim")
+    
+    k.press_and_release("Alt + /")
+    k.write("do " + do_file_name)
+    k.press_and_release("enter")
+    time.sleep(run_to_pane_shift_sleep_sec)
+    k.press_and_release("shift+f4")
+    k.press_and_release("shift+f4")
+    k.press_and_release("f")
+
+auto_run()
+
+
+
