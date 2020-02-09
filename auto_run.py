598a8b85e752f2b463c4cffad288adeddc450dcd
Brandon Valley <bavvh8@mst.edu>
2020-02-09 12:26:37 -0600

    auto_run works better

--- auto_run.py
+++ auto_run.py
@@ -6,7 +6,7 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--do_file_name'               , default = 'run_cmd.do')
-parser.add_argument('-r', '--run_to_pane_shift_sleep_sec', default = 7)
+parser.add_argument('-r', '--run_to_pane_shift_sleep_sec', default = 6)
 args = parser.parse_args()
 
 time.sleep(1)
--- modelsim_utils.py
+++ modelsim_utils.py
@@ -11,13 +11,16 @@ def auto_run(do_file_name = 'run_cmd.do', run_to_pane_shift_sleep_sec = 7):
     
     k.press_and_release("Alt + /")
     k.write("do " + do_file_name)
+    time.sleep(.2)
     k.press_and_release("enter")
     time.sleep(run_to_pane_shift_sleep_sec)
     k.press_and_release("shift+f4")
     k.press_and_release("shift+f4")
     k.press_and_release("f")
 
-auto_run()
+if __name__ == "__main__":
+    auto_run()
 
 
 
+# do run_cmd.do
