524e801cf9d5ab8f2bef6ed7276d2c8792e15cb7
Brandon Valley <bavvh8@mst.edu>
2020-02-15 16:26:52 -0600

    auto_run working

--- modelsim_utils.py
+++ modelsim_utils.py
@@ -32,8 +32,9 @@ import keyboard as k
 import _tkinter 
 
 
-NEXT_PANE__SHORTCUT_STR        = 'Shift+f4'
-RAISE_CMD_WINDOW__SHORTCUT_STR = 'Alt+/'
+NEXT_PANE__SHORTCUT_STR           = 'Shift+f4'
+RAISE_CMD_WINDOW__SHORTCUT_STR    = 'Alt+/'
+ZOOM_FULL__WAVE_PANE_SHORTCUT_STR = 'f'
 
 # press Alt + / when modelsim opens
 # project must be open in modelsim
@@ -66,8 +67,32 @@ def auto_run(do_file_name = 'run_cmd.do', run_to_pane_shift_sleep_sec = 7):
         k.write(cmd)
     
     
-    
+    # run command to execute do file
     k.press_and_release("enter")
+    
+    
+    time.sleep(7)  # not optimized!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+     
+    # shift into terminal pane from the sim pane
+    k.press_and_release(NEXT_PANE__SHORTCUT_STR)
+    
+    # enter command that does nothing because if you don't, you can't get into the wave pane for some reason
+    k.press_and_release('esc') # just in case you had an auto-complete window up before running this
+    k.press_and_release('enter')
+    
+    # shift into wave pane from terminal pane 
+    k.press_and_release(NEXT_PANE__SHORTCUT_STR)
+    
+    # zoom full to show the part of the wave you care about
+    k.press_and_release(ZOOM_FULL__WAVE_PANE_SHORTCUT_STR)
+    
+    
+    
+    
+#     k.write
+    
+    
+    
 #     console_output = aku.make_then_get_selection(deselect_key_str = 'right arrow', error_on_empty_clipboard = True) # sim:/nand4_tb/i_a sim:/nand4_tb/i_b sim:/nand4_tb/i_c sim:/nand4_tb/i_d sim:/nand4_tb/o_f 
 #     print(console_output) # sim:/nand4_tb/duv/NAND2_1/i_a sim:/nand4_tb/duv/NAND2_1/i_b sim:/nand4_tb/duv/NAND2_1/o_f 
  
